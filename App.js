6899ea9e54b482f69684a81eecf506b239533ef7
AkashAgarwa <97722279+AkashAgarwa@users.noreply.github.com>
2022-02-01 15:29:37 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+import React, { Component } from "react";
+import InputContainer from "./components/container";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <InputContainer />
+      </div>
+    );
+  }
+}
